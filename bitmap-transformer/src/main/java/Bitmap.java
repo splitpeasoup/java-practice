2879a364f45b80ebd6487ac347921f5eff18b5c5
splitpeasoup <caitlinsweeneylmt@gmail.com>
2018-11-30 11:10:35 -0800

    creating bitmap class

---
+++ bitmap-transformer/src/main/java/Bitmap.java
@@ -0,0 +1,13 @@
+import java.awt.image.BufferedImage;
+import java.awt.Image;
+
+public class Bitmap extends App{
+
+   BufferedImage input;
+   BufferedImage output;
+   String pathtransform;
+   String name;
+
+
+
+}
